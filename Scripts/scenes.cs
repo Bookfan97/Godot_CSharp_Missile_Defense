1f184476334e7e22e759dd9807d8cd85ce7aa96a
Noah Day <sheltielover432@gmail.com>
2020-09-02 20:53:43 -0400

    Clean Start
    Tilemap

--- Scripts/scenes.cs
+++
@@ -1,8 +0,0 @@
-using Godot;
-using System;
-
-public class scenes : Node
-{
-    public PackedScene _sceneBullet = (PackedScene) GD.Load("res://scenes//bullet.tscn");
-    public PackedScene _sceneExplosion = (PackedScene) GD.Load("res://scenes//explosion.tscn");
-}
