0a26f99376163f9b5d9c414a213267e0b631a56a
Noah Day <sheltielover432@gmail.com>
2020-09-03 21:51:25 -0400

    Health and Scoring System

--- scripts/explosion.cs
+++ scripts/explosion.cs
@@ -4,11 +4,13 @@ using System;
 public class explosion : Area2D
 {
     private bulletBrain _bulletBrain;
+    private player _player;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _bulletBrain = (bulletBrain)GetNode("/root/game/bullets/bulletBrain");
+        _player = (player)GetNode("/root/game/player");
     }
 
     public void _on_AnimatedSprite_animation_finished()
@@ -23,6 +25,7 @@ public class explosion : Area2D
         if ((bulletType != null) && (bulletType.Animation == "enemy") && (bullet is bullet))
         {
             _bulletBrain.spawnExplosion(bullet.GlobalPosition, "enemy");
+            _player.addScore();
             bullet.QueueFree();
         }
     }
--- scripts/player.cs
+++ scripts/player.cs
@@ -5,6 +5,7 @@ public class player : Node
 {
     private bulletBrain _bulletBrain;
     public bool canShoot = true;
+    public int health = 3, score = 0;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -19,13 +20,27 @@ public class player : Node
         {
             _bulletBrain.spawnExplosion(bullet.GlobalPosition, "enemy");
             bullet.QueueFree();
+            hitPlayer();
         }
     }
 
+    public void hitPlayer(int damageAmount = 1)
+    {
+        health = Math.Max(health-damageAmount, 0);
+        updateUI();
+    }
+
+    public void addScore(int scoreAmount = 1)
+    {
+        score = Math.Max(scoreAmount + score, 0);
+        updateUI();
+    }
+    
     public void updateUI()
     {
         var healthAndScore = (Label) GetNode("/root/game/HUD/healthAndScore");
-        healthAndScore.Text = "WHOOPS YOU FORGOT SOMETHING";
+        var newHUDtext = "SCORE: " + score + "       " + "HEALTH: " + health;
+        healthAndScore.Text = newHUDtext;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
