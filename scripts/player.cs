66474246bc372bb28d38911f75b986e34706717a
Noah Day <sheltielover432@gmail.com>
2020-09-03 22:15:25 -0400

    Game Over

--- scripts/player.cs
+++ scripts/player.cs
@@ -4,7 +4,7 @@ using System;
 public class player : Node
 {
     private bulletBrain _bulletBrain;
-    public bool canShoot = true;
+    public bool canShoot = true, gameOver = false;
     public int health = 3, score = 0;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -12,7 +12,15 @@ public class player : Node
         _bulletBrain = (bulletBrain)GetNode("/root/game/bullets/bulletBrain");
         updateUI();
     }
-
+    public override void _Input(InputEvent @event)
+    {
+        //base._Input(@event);
+        if (@event.IsActionPressed("click") && gameOver)
+        {
+            //GD.Print("Left mouse clicked");
+            GetTree().ReloadCurrentScene();
+        }
+    }
     public void _on_playerHitZone_area_entered(Area2D bullet)
     {
         var bulletType = (AnimatedSprite) bullet.GetNodeOrNull("AnimatedSprite");
@@ -28,6 +36,16 @@ public class player : Node
     {
         health = Math.Max(health-damageAmount, 0);
         updateUI();
+        if ((health <= 0) && (gameOver != true))
+        {
+            gameOver = true;
+            canShoot = false;
+            var gameOverScreen = (Node2D) GetNode("/root/game/HUD/gameOverScreen");
+            gameOverScreen.Visible = true;
+            var Cannon = (Node2D) GetNode("/root/game/foreground/cannon");
+            _bulletBrain.spawnExplosion(Cannon.GlobalPosition, "enemy");
+            Cannon.QueueFree();
+        }
     }
 
     public void addScore(int scoreAmount = 1)
